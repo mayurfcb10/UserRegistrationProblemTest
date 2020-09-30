a4cabf7dfa0bdc8fad7a7de8e9c2ac24a87530fb
Mayur <mayurzope555@gmail.com>
2020-09-30 17:16:00 +0530

    [Mayur]Added: Junit Test case on the User Validation Problem

--- src/test/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidationTest.java
@@ -15,4 +15,100 @@ public class UserValidationTest
 		userValidation.PrintWelcomeMessage();
 	}
 
+	@Test 
+	public void Given_Message_When_Sad_Should_Return_Sad() {
+		String mood = userValidation.AnalyseMood("This is a Sad Message");
+		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test 
+	public void Given_Message_When_Happy_Should_Return_Happy() {
+		String mood = userValidation.AnalyseMood("This is a Happy Message");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void given_FirstName_CapitalLetter_return_true()
+	{
+		boolean result = userValidation.ValidateFirstName("Marsh");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_FirstName_withMixedAlphabets_return_false()
+	{
+		boolean result = userValidation.ValidateFirstName("maRsh");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_LastName_Alphanumeric_return_false()
+	{
+		boolean result = userValidation.ValidateFirstName("Mar6sh");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_LastNamewithCharaters_return_false()
+	{
+		boolean result = userValidation.ValidateLastName("ma&%#&HFrsh");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_Email_when_given_CountryDomain_return_true()
+	{
+		boolean result = userValidation.ValidateEmail("abc@abc.com.au");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_Email_when_emailStartswithDot_return_false()
+	{
+		boolean result = userValidation.ValidateEmail(".abc@gmail.com");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_Email_containsOptional_Special_Character_return_true()
+	{
+		boolean result = userValidation.ValidateEmail("abc@%*.com.com");
+		Assert.assertEquals( false, result );
+	}
+
+	@Test
+	public void given_PhoneNumber_with_valid_countryCode_return_true()
+	{
+		boolean result = userValidation.ValidatePhoneNumber("91 7972245761");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_PhoneNumber_with_Invalid_countryCode_return_false()
+	{
+		boolean result = userValidation.ValidatePhoneNumber("7596859453");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_correct_return_true()
+	{
+		boolean result = userValidation.ValidatePassword("zcsfkasHds98&");
+		Assert.assertTrue( result);
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_incorrect_return_false()
+	{
+		boolean result = userValidation.ValidatePassword("zcsfkasds98&");
+		Assert.assertFalse( result);
+	}
+
+	@Test
+	public void given_Strong_Password_when_given_more_than_one_Specialcharacter_return_false()
+	{
+		boolean result = userValidation.ValidatePassword("zcsfkasds9&5%8&");
+		Assert.assertEquals( result, false);
+	}
+
 }
