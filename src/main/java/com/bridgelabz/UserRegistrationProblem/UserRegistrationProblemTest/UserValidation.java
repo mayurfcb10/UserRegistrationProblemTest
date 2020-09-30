ab5dd11053de87e5f6933f21ad81a4ea7c22bf37
Mayur <mayurzope555@gmail.com>
2020-09-30 17:51:31 +0530

    [Mayur]Added: First Name with Unit Test Case Validation

--- src/main/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidation.java
+++ src/main/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidation.java
@@ -1,11 +1,19 @@
-/* Welcome to the User Registration Problem test */
-
 package com.bridgelabz.UserRegistrationProblem.UserRegistrationProblemTest;
 
 public class UserValidation {
 
+	public String AnalyseMood(String message) {
+		if(message.contains("Sad"))
+			return "SAD";
+		return "HAPPY";
+	}
+	
 	public void PrintWelcomeMessage() {
 		System.out.println("Welcome to the User Validation Problem");
+	}
+
+	public boolean ValidateFirstName(String firstName) {
+		return firstName.matches("([A-Z][a-z]{2,})");
 	}
 
 }
--- src/test/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidationTest.java
@@ -15,4 +15,38 @@ public class UserValidationTest
 		userValidation.PrintWelcomeMessage();
 	}
 
+	@Test 
+	public void Given_Message_When_Sad_Should_Return_Sad() {
+		String mood = userValidation.AnalyseMood("This is a Sad Message");
+		Assert.assertEquals("SAD", mood);
+	}
+
+	@Test 
+	public void Given_Message_When_Happy_Should_Return_Happy() {
+		String mood = userValidation.AnalyseMood("This is a Happy Message");
+		Assert.assertEquals("HAPPY", mood);
+	}
+
+	@Test
+	public void given_FirstName_CapitalLetter_return_true()
+	{
+		boolean result = userValidation.ValidateFirstName("Marsh");
+		Assert.assertTrue( result );
+	}
+
+	@Test
+	public void given_FirstName_withMixedAlphabets_return_false()
+	{
+		boolean result = userValidation.ValidateFirstName("maRsh");
+		Assert.assertFalse( result );
+	}
+
+	@Test
+	public void given_FirstName_Alphanumeric_return_false()
+	{
+		boolean result = userValidation.ValidateFirstName("Mar6sh");
+		Assert.assertFalse( result );
+	}
+
+	
 }
