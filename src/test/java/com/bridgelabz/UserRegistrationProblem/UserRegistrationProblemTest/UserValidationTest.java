b96b3b436ae8345086a6eabed23461c3b19e8466
Mayur <mayurzope555@gmail.com>
2020-09-30 18:26:02 +0530

    [Mayur]Added: Password Rule 1 to be tested with Junit Test Case

--- src/test/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationProblem/UserRegistrationProblemTest/UserValidationTest.java
@@ -105,24 +105,24 @@ public class UserValidationTest
 	}
 	
 	@Test
-	public void given_Strong_Password_when_given_correct_return_true()
+	public void given_Strong_Password_when_given_CapitalAlphabet_return_true()
 	{
-		boolean result = userValidation.ValidatePassword("zcsfkasHds98&");
+		boolean result = userValidation.ValidatePassword("sfasHd8&");
 		Assert.assertTrue( result );
 	}
 
 	@Test
-	public void given_Strong_Password_when_given_incorrect_return_true()
+	public void given_Strong_Password_when_given_nonCapitalLetter_return_false()
 	{
 		boolean result = userValidation.ValidatePassword("zcsfkasds98&");
-		Assert.assertEquals( result, true );
+		Assert.assertEquals( result, false );
 	}
 
 	@Test
-	public void given_Strong_Password_when_given_more_than_one_Specialcharacter_return_true()
+	public void given_Strong_Password_when_given_more_than_one_Specialcharacter_return_false()
 	{
 		boolean result = userValidation.ValidatePassword("zcsfkasds9&5%8&");
-		Assert.assertTrue( true );
+		Assert.assertFalse( result );
 	}
 	
 }
